7235b9deba1d673be103be4b6974f703ca2b46a6
lin520jun <2971300989@qq.com>
2025-12-10 21:41:53 +0800

    remove mysql and counter, keep pay notify only

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+// const { init: initDB, Counter } = require("./db");
 
 const logger = morgan("tiny");
 
@@ -17,21 +17,6 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
 
 // 微信支付回调
 app.post("/pay/notify", async (req, res) => {
@@ -45,15 +30,6 @@ app.post("/pay/notify", async (req, res) => {
 });
 
 
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
 // 小程序调用，获取微信 Open ID
 app.get("/api/wx_openid", async (req, res) => {
   if (req.headers["x-wx-source"]) {
@@ -63,11 +39,9 @@ app.get("/api/wx_openid", async (req, res) => {
 
 const port = process.env.PORT || 80;
 
-async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
-}
+app.listen(port, () => {
+  console.log("wxcloudrun-express-p listening on port", port);
+});
+
 
 bootstrap();
